65220e7c57e4827b4153699ef68f0c56d1887723
github username <github email address>
2019-01-20 13:44:36 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body);
+  } else {
+    return new Handlebars.SafeString(`<strong>${this.body}</strong>`)
+  }
+})
+
+function loadIssues() {
+  const template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  let result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
+
+//  Very slow, re-allocates "result" and queries DOM in each loop
+
+// function loadIssue() {
+//   const template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+//   for(let i = 0; i < issues.length; i++) {
+//     let result = template(issues[i]);
+//     document.getElementsByTagName("main")[0].innerHTML += result;
+//   }
+//
+// }
