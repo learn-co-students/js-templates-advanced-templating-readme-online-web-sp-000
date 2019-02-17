787586b30019b1d8665ae02baf448ea6bc829629
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-17 10:30:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+Handlebars.registerHelper('comment_body', function() {
+  let tags = this.state === 'closed' ? ["",""] : ["<strong>","</strong>"]
+  return new Handlebars.SafeString(tags[0]+this.body+tags[1])
+  if (this.state === 'closed') {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString(`<strong>${this.body}</strong>`)
+  }
+})
 
+function loadIssues() {
+  var template = Handlebars.compile(document.getElementById('issue-template').innerHTML)
+  var result = template(issues)
+  document.getElementsByTagName('main')[0].innerHTML += result
+}
+
+function loadIssue() {
+  var issue = {
+    state: "closed",
+    number: 5,
+    created_at: "2006-03-31 16:32:13 UTC",
+    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+  }
+
+  var template = Handlebars.compile(document.getElementById('issue-template').innerHTML)
+  var result = template(issue)
+  document.getElementsByTagName('main')[0].innerHTML += result
+}
